c35fff6d39b34255fe87a72c71dff0a6c44f2b04
Nghiad <nghiad@live.com>
2022-01-01 14:51:39 -0800

    Update VLSM Subnetter.py

--- VLSM Subnetter.py
+++ VLSM Subnetter.py
@@ -163,6 +163,7 @@ if __name__ == "__main__":
         subnetrange.append(subneting_range(i))
 
     for i in range(len(subnetrange)):                               #Subnetting function
+        print ('{}.{}.{}.{} {}'.format(octet1, octet2, octet3, octet4, subnetmasks[i]))
         octet4 = octet4 + subnetrange[i]
         while octet4 > 255:
             octet4 = octet4 - 256
@@ -173,4 +174,5 @@ if __name__ == "__main__":
                 while octet2 > 255:
                     octet2 = octet2 - 256
                     octet1 += 1
-        print ('{}.{}.{}.{} {}'.format(octet1, octet2, octet3, octet4, subnetmasks[i]))
+    input()
+    
